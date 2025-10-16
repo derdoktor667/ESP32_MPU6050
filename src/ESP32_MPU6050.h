3d58640d4a687aa5f322de08358fe6815edc8eb4
Wastl Kraus <derdoktor667@users.noreply.github.com>
2025-10-16 15:38:46 +0200

    add more filtering settings

--- src/ESP32_MPU6050.h
+++ src/ESP32_MPU6050.h
@@ -11,9 +11,23 @@
 #define MPU6050_ACCEL_XOUT_H 0x3B
 #define MPU6050_TEMP_OUT_H 0x41
 #define MPU6050_GYRO_XOUT_H 0x43
+#define MPU6050_USER_CTRL 0x6A
 #define MPU6050_PWR_MGMT_1 0x6B
+#define MPU6050_FIFO_COUNTH 0x72
+#define MPU6050_FIFO_R_W 0x74
 #define MPU6050_WHO_AM_I 0x75
-#define MPU6050_WHO_AM_I_EXPECTED_VALUE 0x70
+#define MPU6050_WHO_AM_I_EXPECTED_VALUE 0x68
+
+// FIFO Enable Register
+#define MPU6050_FIFO_EN 0x23
+#define MPU6050_FIFO_EN_ACCEL_BIT 3
+#define MPU6050_FIFO_EN_GYRO_X_BIT 6
+#define MPU6050_FIFO_EN_GYRO_Y_BIT 5
+#define MPU6050_FIFO_EN_GYRO_Z_BIT 4
+
+// User Control Register Bits
+#define MPU6050_USER_CTRL_FIFO_EN_BIT 6
+#define MPU6050_USER_CTRL_FIFO_RESET_BIT 2
 
 // MPU6050 Power Management 1 Register values
 #define MPU6050_PWR_MGMT_1_WAKE 0x00
@@ -60,6 +74,18 @@ enum AccelRange
   ACCEL_RANGE_16G
 };
 
+// Enums for Digital Low Pass Filter (DLPF) bandwidths
+enum LpfBandwidth
+{
+  LPF_256HZ_N_0MS = 0, // 256 Hz, 0ms delay
+  LPF_188HZ_N_2MS = 1, // 188 Hz, 2ms delay
+  LPF_98HZ_N_3MS = 2,  // 98 Hz, 3ms delay
+  LPF_42HZ_N_5MS = 3,  // 42 Hz, 5ms delay
+  LPF_20HZ_N_10MS = 4, // 20 Hz, 10ms delay
+  LPF_10HZ_N_13MS = 5, // 10 Hz, 13ms delay
+  LPF_5HZ_N_18MS = 6   // 5 Hz, 18ms delay
+};
+
 // Struct for 3-axis floating point data.
 struct AxisData
 {
@@ -82,13 +108,15 @@ public:
 
   ESP32_MPU6050(int8_t address = MPU6050_ADDR);
 
-  bool begin(GyroRange gyroRange = GYRO_RANGE_250DPS, AccelRange accelRange = ACCEL_RANGE_2G);
+  bool begin(GyroRange gyroRange = GYRO_RANGE_2000DPS, AccelRange accelRange = ACCEL_RANGE_16G, LpfBandwidth lpfBandwidth = LPF_42HZ_N_5MS);
   bool setGyroscopeRange(GyroRange range);
   bool setAccelerometerRange(AccelRange range);
+  bool setLpfBandwidth(LpfBandwidth bandwidth);
   void calibrate(int num_samples = 1000);
   bool update();
 
 private:
+  void initFIFO();
   bool writeRegister(uint8_t reg, uint8_t value);
   bool readRegisters(uint8_t reg, uint8_t count, uint8_t *dest);
   uint8_t readRegister(uint8_t reg);
