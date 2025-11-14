f08fd4ddbaeba1f23985d7d85d5c78da90cbf2ba
Wastl Kraus <derdoktor667@users.noreply.github.com>
2025-11-14 13:00:01 +0100

    Create ESP32_MPU6050.h

---
+++ ESP32_MPU6050.h
@@ -0,0 +1,12 @@
+/**
+ * @file ESP32_MPU6050.h
+ * @brief Wrapper / Adapter File
+ * @author Wastl Kraus
+ * @date 2025-10-17
+ * @license MIT
+ */
+
+#pragma once
+
+#include <Arduino.h>
+#include "src/ESP32_MPU6050.h"
